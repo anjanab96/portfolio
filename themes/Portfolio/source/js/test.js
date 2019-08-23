78de43135358447deff20d7ca898c2bdbca3923f
Anjana Bala <anjana.bala96@gmail.com>
2019-08-23 19:58:11 -0400

    case study page changes/ about page changes

--- themes/Portfolio/source/js/test.js
+++ themes/Portfolio/source/js/test.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+
+    $('.carousel').each(function(index, element) {
+        var slider = $(this);
+        slider.slick({
+            dots:true, 
+            arrows: true, 
+            autoplay: true, 
+            slidesToShow: 2,
+        })
+        
+    });
+
+
+    /*
+    $('.carousel').slick( {
+        dots: true, 
+        arrows: true, 
+        autoplay: true, 
+        slidesToShow: 2,
+    
+    });
+    */ 
+    
   
 })
